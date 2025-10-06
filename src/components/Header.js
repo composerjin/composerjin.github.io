43058697ac28cf68e7844719f2383b85b64ace8a
조현진 <hyunjincho@johyeonjin-ui-MacBookPro.local>
2025-10-06 23:07:43 +0100

    compression and pfp change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './Header.css';
 import Socials from './Socials';
-const bgImage = process.env.PUBLIC_URL + '/headerimg.jpg';
+const bgImage = process.env.PUBLIC_URL + '/headerimg.webp';
 
 const Header = () => {
   return (
@@ -13,7 +13,7 @@ const Header = () => {
       <h1>Composer Jin</h1>
       <h2>Music Composer for Film, Game & Visual Media</h2>
       <div className="profile-pic">
-        <img src="portrait.jpg" alt="Composer Jin" />
+        <img src="portrait.webp" alt="Composer Jin" />
       </div>
 
       <div className="header-socials">
