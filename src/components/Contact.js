3e9e6c44f620b71eb6c1167a668759265aed2f67
Paul Revell <evilxxpaul@hotmail.co.uk>
2025-10-10 13:20:01 +0100

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,14 +30,32 @@ const Contact = ({ forceOpen }) => {
           <div className="modal-content" onClick={e => e.stopPropagation()}>
             <button className="modal-close" onClick={closeModal}>×</button>
             <h3>Contact Me</h3>
-            <p>If you'd like to get in touch, please fill out the form below or reach out via email.</p>
+            <p>If you'd like to get in touch, please fill out the form below or reach out via email (contact@composer-jin.com)</p>
 
-            <form className="contact-form">
-              <input type="text" placeholder="Your Name" required />
-              <input type="email" placeholder="Your Email" required />
-              <textarea placeholder="Your Message" rows="5" required></textarea>
+            <form
+              className="contact-form"
+              action="https://formspree.io/f/mjkaarbk"
+              method="POST"
+            >
+              <input type="hidden" name="_subject" value="Contact Form Submission" />
+
+              {/* <label htmlFor="submissionType">Type of Submission</label> */}
+              <select name="submissionType" required>
+                <option value="">-- Type of Enquiry --</option>
+                <option value="purchase-existing">Purchase an existing song</option>
+                <option value="commission-new">Commission a new song</option>
+                <option value="collab">Collaboration</option>
+                <option value="general">Other enquiries</option>
+              </select>
+
+              <input type="text" name="name" placeholder="Your Name" required />
+              <input type="email" name="email" placeholder="Your Email" required />
+              {/* <input type="hidden" name="_next" value="http://localhost:3000/#/contact" /> */}
+              <textarea name="message" placeholder="Your Message" rows="5" required></textarea>
               <button type="submit">Send Message</button>
             </form>
+
+
           </div>
         </div>
       )}
